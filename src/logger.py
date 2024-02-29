76bf5016b735e7a6a35f860e6d675909a0dae505
Richard Decal <public@richarddecal.com>
2024-02-29 15:07:42 -0500

    Apply ruff formatter to repo
    Signed-off-by: Richard Decal <public@richarddecal.com>

--- src/logger.py
+++ src/logger.py
@@ -7,7 +7,7 @@ logger.setLevel(logging.DEBUG)  # Set the threshold for this logger to DEBUG or 
 # Add the log message handler to the logger
 handler = logging.StreamHandler()  # Writes logging output to streams like sys.stdout, sys.stderr or any file-like object
 
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 handler.setFormatter(formatter)
 
 logger.addHandler(handler)
--- src/prep/build_opensearch_index.py
+++ src/prep/build_opensearch_index.py
@@ -1,6 +1,7 @@
 """
 Reads vault dictionary and indexes documents into an opensearch index.
 """
+
 import pickle
 
 from opensearchpy import OpenSearch, RequestsHttpConnection
@@ -8,11 +9,12 @@ from opensearchpy.helpers import bulk
 
 from src.logger import logger
 
-INDEX_NAME = 'obsidian-vault'
+INDEX_NAME = "obsidian-vault"
 
 
-def get_opensearch(host: str = 'opensearch') -> OpenSearch:
-    """Create an opensearch client.
+def get_opensearch(host: str = "opensearch") -> OpenSearch:
+    """
+    Create an opensearch client.
 
     Args:
         host: Name of opensearch host. Defaults to 'opensearch'.
@@ -21,80 +23,65 @@ def get_opensearch(host: str = 'opensearch') -> OpenSearch:
         Opensearch client
     """
     port = 9200
-    auth = ('admin', 'admin')
+    auth = ("admin", "admin")
 
     client = OpenSearch(
-        hosts=[{'host': host, 'port': port}],
+        hosts=[{"host": host, "port": port}],
         connection_class=RequestsHttpConnection,
         http_compress=True,  # enables gzip compression for request bodies
         http_auth=auth,
         use_ssl=True,
         verify_certs=False,
         ssl_assert_hostname=False,
-        ssl_show_warn=False
+        ssl_show_warn=False,
     )
     return client
 
 
 def create_index(client: OpenSearch, index_name: str) -> None:
-    """Create opensearch index with custom analyzer
+    """
+    Create opensearch index with custom analyzer.
 
     Args:
         client: Opensearch client
         index_name: Name of opensearch index
     """
     index_body = {
-        'settings': {
-            'analysis': {
-                'char_filter': {
-                    'html_strip_filter': {
-                        'type': 'html_strip'
-                    }
-                },
-                'filter': {
-                    'english_possessive_stemmer': {
-                        'type': 'stemmer',
-                        'language': 'possessive_english'
+        "settings": {
+            "analysis": {
+                "char_filter": {"html_strip_filter": {"type": "html_strip"}},
+                "filter": {
+                    "english_possessive_stemmer": {
+                        "type": "stemmer",
+                        "language": "possessive_english",
                     },
-                    'english_stop': {
-                        'type': 'stop',
-                        'stopwords': '_english_'
-                    },
-                    'english_stemmer': {
-                        'type': 'stemmer',
-                        'language': 'english'
-                    }
+                    "english_stop": {"type": "stop", "stopwords": "_english_"},
+                    "english_stemmer": {"type": "stemmer", "language": "english"},
                 },
-                'analyzer': {
-                    'english_custom': {
-                        'tokenizer': 'standard',
-                        'filter': [
-                            'english_possessive_stemmer',
-                            'lowercase',
-                            'english_stop',
-                            'english_stemmer'
+                "analyzer": {
+                    "english_custom": {
+                        "tokenizer": "standard",
+                        "filter": [
+                            "english_possessive_stemmer",
+                            "lowercase",
+                            "english_stop",
+                            "english_stemmer",
                         ],
-                        'char_filter': [
-                            'html_strip_filter'
-                        ]
+                        "char_filter": ["html_strip_filter"],
                     }
-                }
+                },
             },
-            'index': {
-                'query': {
-                    'default_field': 'chunk'
-                }
+            "index": {"query": {"default_field": "chunk"}},
+        },
+        "mappings": {
+            "properties": {
+                "title": {"type": "text", "analyzer": "english_custom"},
+                "type": {"type": "keyword"},
+                "path": {"type": "keyword"},
+                "chunk_header": {"type": "text", "analyzer": "english_custom"},
+                "chunk": {"type": "text", "analyzer": "english_custom"},
             }
         },
-        'mappings': {
-            'properties': {
-                'title': {'type': 'text', 'analyzer': 'english_custom'},
-                'type': {'type': 'keyword'},
-                'path': {'type': 'keyword'},
-                'chunk_header': {'type': 'text', 'analyzer': 'english_custom'},
-                'chunk': {'type': 'text', 'analyzer': 'english_custom'},
-            }
-        }
     }
 
     # Iterate over all rows in df and add to index
@@ -103,7 +90,8 @@ def create_index(client: OpenSearch, index_name: str) -> None:
 
 
 def index_vault(vault: dict[str, dict], client: OpenSearch, index_name: str) -> None:
-    """Index vault into opensearch index
+    """
+    Index vault into opensearch index.
 
     Args:
         vault: Obsidian vault dictionary
@@ -115,17 +103,28 @@ def index_vault(vault: dict[str, dict], client: OpenSearch, index_name: str) -> 
     docs = []
 
     for chunk_id, doc in vault.items():
-        path = doc['path']
-        title = doc['title']
-        doc_type = doc['type']
-        chunk = doc['chunk']
+        path = doc["path"]
+        title = doc["title"]
+        doc_type = doc["type"]
+        chunk = doc["chunk"]
         chunk_header = chunk[0]
         docs_indexed += 1
         if docs_indexed % 100 == 0:
-            logger.info(f'Indexing {chunk_id} - Path: {path} (progress: {docs_indexed:,} docs)')
+            logger.info(
+                f"Indexing {chunk_id} - Path: {path} (progress: {docs_indexed:,} docs)"
+            )
 
-        docs.append({'_index': index_name, '_id': chunk_id, 'title': title, 'type': doc_type,
-                     'path': path, 'chunk_header': chunk_header, 'chunk': chunk})
+        docs.append(
+            {
+                "_index": index_name,
+                "_id": chunk_id,
+                "title": title,
+                "type": doc_type,
+                "path": path,
+                "chunk_header": chunk_header,
+                "chunk": chunk,
+            }
+        )
         chunks_indexed += 1
 
         if chunks_indexed % 2000 == 0:
@@ -135,11 +134,18 @@ def index_vault(vault: dict[str, dict], client: OpenSearch, index_name: str) -> 
     if chunks_indexed % 200 > 0:
         bulk(client, docs)
 
-    logger.info(f'Indexed {chunks_indexed:,} chunks (including full documents)')
+    logger.info(f"Indexed {chunks_indexed:,} chunks (including full documents)")
 
 
-def query_opensearch(query: str, client: OpenSearch, index_name: str, type: str = 'chunk', n_results: int = 10) -> dict:
-    """Custom query for opensearch index.
+def query_opensearch(
+    query: str,
+    client: OpenSearch,
+    index_name: str,
+    type: str = "chunk",
+    n_results: int = 10,
+) -> dict:
+    """
+    Custom query for opensearch index.
 
     Args:
         query: Query string
@@ -152,43 +158,31 @@ def query_opensearch(query: str, client: OpenSearch, index_name: str, type: str 
         Result of opensearch query
     """
     query = {
-        'size': n_results,
-        'query': {
-            'bool': {
-                'should': [
+        "size": n_results,
+        "query": {
+            "bool": {
+                "should": [
                     {
-                        'match': {
-                            'title': {
-                                'query': query,
-                                'boost': 5.0  # Give matches in the 'title' field a higher score
+                        "match": {
+                            "title": {
+                                "query": query,
+                                "boost": 5.0,  # Give matches in the 'title' field a higher score
                             }
                         }
                     },
                     {
-                        'match': {
-                            'chunk_header': {
-                                'query': query,
-                                'boost': 2.0  # Give matches in the 'chunk_header' field a higher score
+                        "match": {
+                            "chunk_header": {
+                                "query": query,
+                                "boost": 2.0,  # Give matches in the 'chunk_header' field a higher score
                             }
                         }
                     },
-                    {
-                        'match': {
-                            'chunk': {
-                                'query': query
-                            }
-                        }
-                    }
+                    {"match": {"chunk": {"query": query}}},
                 ],
-                'filter': [
-                    {
-                        'term': {
-                            'type': 'chunk'
-                        }
-                    }
-                ]
+                "filter": [{"term": {"type": "chunk"}}],
             }
-        }
+        },
     }  # type: ignore
 
     response = client.search(index=index_name, body=query)
@@ -196,14 +190,14 @@ def query_opensearch(query: str, client: OpenSearch, index_name: str, type: str 
     return response
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Load vault dictionary
-    vault = pickle.load(open('data/vault_dict.pickle', 'rb'))
-    logger.info(f'Vault length: {len(vault):,}')
+    vault = pickle.load(open("data/vault_dict.pickle", "rb"))
+    logger.info(f"Vault length: {len(vault):,}")
 
     # Create client
     client = get_opensearch()
-    logger.info(f'Client: {client.info()}')
+    logger.info(f"Client: {client.info()}")
 
     # Create index
     create_index(client, INDEX_NAME)
@@ -212,10 +206,12 @@ if __name__ == '__main__':
     index_vault(vault, client, INDEX_NAME)
 
     # Count the number of documents in the index
-    logger.info(f'Client cat count: {client.cat.count(index=INDEX_NAME, params={"format": "json"})}')
+    logger.info(
+        f'Client cat count: {client.cat.count(index=INDEX_NAME, params={"format": "json"})}'
+    )
 
     # Test query
-    test_query = 'Examples of bandits in industry'
+    test_query = "Examples of bandits in industry"
     response = query_opensearch(test_query, client, INDEX_NAME)
-    logger.info(f'Test query: {test_query}')
-    logger.info(f'Response: {response}')
+    logger.info(f"Test query: {test_query}")
+    logger.info(f"Response: {response}")
